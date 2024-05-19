0613b58a5d64c1f94c91696e6c22a72c677c2e9c
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-05-19 13:59:23 +0300

    zadaniye 4

---
+++ build/third-party/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/third-party/gtest/googletest
+# Build directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/build/third-party/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
