8bcbe404ab1a2d0ec6b105df1f5fd27691aad68d
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-05-19 14:02:23 +0300

    zadaniye 4

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory
-# Build directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/build/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/CMakeLists.txt;24;add_test;/home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
--- build/third-party/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/third-party/gtest/googletest
-# Build directory: /home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/build/third-party/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
