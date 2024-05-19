f1c7c44933af741b8d7471c933ee79c9e1975ff2
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-05-19 13:27:58 +0300

    zadaniye 4

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,11 @@
-#include <ConceptualExample.cpp>
+#include </home/timur/BaltaevTimur/workspace/rk2/AbstractFactory/ConceptualExample.cpp>
 #include <gtest/gtest.h>
 
 TEST(ConcreteProductA1, example){
 	ConcreteProductA1 test;
 	ASSERT_EQ(test.usefulFunctionA(), "The result of the product A1.");	
 }
-TEST(ConcreteProductA2, example){
-	ConcreteProductA2 test;
-	ASSERT_EQ(test.usefulFunctionA(), "The result of the product A2.");	
-}
+
 TEST(ConcreteProductB1, example){
 	ConcreteProductB1 test;
 	ASSERT_EQ(test.usefulFunctionB(), "The result of the product B1.");	
